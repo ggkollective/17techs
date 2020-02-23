8501aaca354742107083fc0548b9de61d3a2681d
gigone.lee <gigone.lee@gmail.com>
2020-02-23 16:30:45 +0900

    parser 명시

--- part2/ch10_xml/xml_reader.py
+++ part2/ch10_xml/xml_reader.py
@@ -15,7 +15,7 @@ def read_xpath(tree, xpath):
 def open_xml_file(filename):
     with open(filename, encoding='UTF8') as file:
         try:
-            return etree.parse(file)
+            return etree.parse(file, parser=etree.XMLParser(encoding='utf-8'))
         except KeyError as e:
             print('XML 데이터를 파싱하는 데 실패했습니다. 사유={0}'.format(e))
             return None
